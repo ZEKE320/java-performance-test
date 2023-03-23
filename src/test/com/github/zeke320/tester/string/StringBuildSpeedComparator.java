da8cef3322e7e61cde276db762a35701065549d1
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-03-23 14:47:40 +0900

    ✅ 追加｜Stringの結合速度を比較するクラスを追加

---
+++ src/test/com/github/zeke320/tester/string/StringBuildSpeedComparator.java
@@ -0,0 +1,60 @@
+package test.com.github.zeke320.tester.string;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class StringBuildSpeedComparator {
+
+  private static void runWithPlusOperator(List<String> list) {
+    String str1 = "abc" + "abc" + "abc";
+    list.add(str1);
+  }
+
+  private static void runWithRecursivePlusOperator(List<String> list) {
+    String str2 = "";
+    for (int i = 0; i < 3; i++) {
+      str2 += "abc";
+    }
+    list.add(str2);
+  }
+
+  private static void runWithStringBuilder(List<String> list) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < 3; i++) {
+      stringBuilder.append("abc");
+    }
+    list.add(stringBuilder.toString());
+  }
+
+  public static void main(String[] args) throws Exception {
+    List<String> strList1 = new ArrayList<>();
+    List<String> strList2 = new ArrayList<>();
+    List<String> strList3 = new ArrayList<>();
+
+    long time1 = System.nanoTime();
+
+    IntStream.rangeClosed(0, 10000).forEach(n -> runWithPlusOperator(strList1));
+    long time2 = System.nanoTime();
+
+    IntStream.rangeClosed(0, 10000).forEach(n -> runWithRecursivePlusOperator(strList2));
+    long time3 = System.nanoTime();
+
+    IntStream.rangeClosed(0, 10000).forEach(n -> runWithStringBuilder(strList3));
+    long time4 = System.nanoTime();
+
+    System.out.println(time2 - time1);
+    System.out.println(time3 - time2);
+    System.out.println(time4 - time3);
+
+    IntStream.rangeClosed(0, 10000).forEach(index -> {
+      if (Objects.equals(strList1.get(index), strList2.get(index))
+          && Objects.equals(strList2.get(index), strList3.get(index))) {
+        return;
+      }
+      throw new IllegalStateException();
+    });
+  }
+}
+
